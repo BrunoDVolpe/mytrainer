4ca711f49f7a955f1a348d7fbc2ee887f5dea032
Bruno Volpe <bdva16@hotmail.com>
2023-06-24 22:46:04 -0300

    Implemented password change

--- users/urls.py
+++ users/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', views.homeView, name='home'),
     path('profile/', views.userProfileView, name='profile'),
     path('profile/update/', views.userProfileUpdateView, name='profile_update'),
+    path('change_password/', views.change_password, name='change_password'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import CustomUserRegisterForm, LoginForm, UserProfileUpdateForm
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib.auth.models import User, Permission
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
+from .forms import CustomUserRegisterForm, LoginForm, UserProfileUpdateForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # Create your views here.
@@ -76,3 +77,22 @@ def userProfileUpdateView(request):
         'form': form,
     }
     return render(request, 'profile_update.html', context)
+
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    context = {
+        'form': form
+    }
+    return render(request, 'auth/change_password.html', context)
